b0e973bc1686432dec54fa2471c5a1c4907f8fb5
dongmyoungLee <pajang1515@daum.net>
2023-04-07 11:27:28 +0900

    HttpRequests..

---
+++ httpRequest/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import MoviesList from './components/MoviesList';
+import './App.css';
+
+function App() {
+
+  function fetchMoivesHandler() {
+
+  }
+
+  return (
+    <React.Fragment>
+      <section>
+        <button>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={dummyMovies} />
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
